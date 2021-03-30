b47861f68e4c4a16e3019c911fcc84d901538329
srinivas <rapolusrinvas7@gmail.com>
2021-03-30 18:43:03 +0530

    this is srinivas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,22 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AmenitiesComponent } from './amenities/amenities.component';
+import { DescriptionComponent } from './description/description.component';
+import { GalleryComponent } from './gallery/gallery.component';
+import { HomeComponent } from './home/home.component';
+import { LocationDetailsComponent } from './location-details/location-details.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { SiteLayoutComponent } from './site-layout/site-layout.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'',component:HomeComponent},
+  {path:'description',component:DescriptionComponent},
+  {path:'amenities',component:AmenitiesComponent},
+  {path:'sitelayout',component:SiteLayoutComponent},
+  {path:'location',component:LocationDetailsComponent},
+  {path:'gallery',component:GalleryComponent},
+  {path:'**',component:PageNotFoundComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,38 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { CarouselModule } from 'ngx-owl-carousel-o';
+import { Routes, RouterModule } from '@angular/router';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DescriptionComponent } from './description/description.component';
+import { AmenitiesComponent } from './amenities/amenities.component';
+import { SiteLayoutComponent } from './site-layout/site-layout.component';
+import { LocationDetailsComponent } from './location-details/location-details.component';
+import { GalleryComponent } from './gallery/gallery.component';
+import { HomeComponent } from './home/home.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DescriptionComponent,
+    AmenitiesComponent,
+    SiteLayoutComponent,
+    LocationDetailsComponent,
+    GalleryComponent,
+    HomeComponent,
+    PageNotFoundComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserModule,
+    CarouselModule,
+    BrowserAnimationsModule,
+     RouterModule.forRoot([]),
   ],
-  providers: [],
+  providers: [GalleryComponent ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
